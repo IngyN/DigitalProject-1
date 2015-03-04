2b19faa69ee3022886efdcc622d92eed445aef1e
AliaMYH <aliahassan95@aucegypt.edu>
2015-03-04 16:02:53 +0200

    qs

--- Table1.h
+++ Table1.h
@@ -9,18 +9,22 @@
 #ifndef __DigitalProject_1__Table1__
 #define __DigitalProject_1__Table1__
 
+#include "Implicant.h"
 #include <string>
 using namespace std;
 
 class Table1
 {
 private:
-    //which data structure? another set >> a set of sets?
     
+    //which data structure? another set >> a set of implicants?
+    //how do we account for the different 'levels'?
     
 public:
+    void traverseAndCompare();
     //function to traverse column and compare in the 'initial'
-    //function to call the implicant combine function and shift implicant to 'intermediate'
+    //function to call the implicant combine function and shift implicant to 'intermediate'?
+    //function to reset all combined implicants in 'intermediate' column to uncombined ie return to initial stage
     
     
 };
